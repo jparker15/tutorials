4afbad649866ca129c434aa106ddebd9a131652a
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-26 23:38:16 -0400

    add ThemeContextProvider

--- react-context-hooks/clientele/src/App.js
+++ react-context-hooks/clientele/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Booklist from './Components/Booklist';
 import Navbar from "./Components/Navbar"
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Booklist/>
+      <ThemeContextProvider>
+        <Navbar/>
+        <Booklist/>
+      </ThemeContextProvider>
+     
     </div>
   );
 }
