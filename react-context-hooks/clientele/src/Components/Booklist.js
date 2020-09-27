82a1d65f3fd2118c8cdcf883de2daa0174229502
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-26 22:38:39 -0400

    create class Component returns a unordered list of books

---
+++ react-context-hooks/clientele/src/Components/Booklist.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Booklist extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>the name of the wind</li>
+                    <li>ender's game</li>
+                    <li>absolute sandman</li>
+                </ul>
+            </div>
+        )
+    }
+}
