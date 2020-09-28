a567c75fafce1a0f2924780e1f6c77214d8ea065
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-27 23:58:16 -0400

    add ThemeContext, set ui to dark mode

--- react-context-hooks/clientele/src/Components/Booklist.js
+++ react-context-hooks/clientele/src/Components/Booklist.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 export default class Booklist extends Component {
+    static contextType = ThemeContext;
     render() {
+        const {isLightTheme,light,dark} = this.context;
+        const theme = !isLightTheme ? light: dark;
         return (
-            <div className="book-list">
+            <div className="book-list"
+                style={{color:theme.syntax,background:theme.bg}}
+            >
                 <ul>
-                    <li>the name of the wind</li>
-                    <li>ender's game</li>
-                    <li>absolute sandman</li>
+                    <li style={{background:theme.bg}}>the name of the wind</li>
+                    <li style={{background:theme.bg}}>ender's game</li>
+                    <li style={{background:theme.bg}}> absolute sandman</li>
                 </ul>
             </div>
         )
