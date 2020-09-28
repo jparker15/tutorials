97a8c73a31245c915af18244182c4b8b97429b83
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-28 17:27:56 -0400

    add Theme toggle, AuthContextProvider

--- react-context-hooks/clientele/src/App.js
+++ react-context-hooks/clientele/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Booklist from './Components/Booklist';
 import Navbar from "./Components/Navbar"
+import ThemeToggle from './Components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar/>
-        <Booklist/>
+        <AuthContextProvider>
+          <Navbar/>
+          <Booklist/>
+          <ThemeToggle/>
+        </AuthContextProvider>
       </ThemeContextProvider>
      
     </div>
