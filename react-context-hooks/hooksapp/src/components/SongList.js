8a96f9fd7072151853b400be8917d1274a1c9fd9
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-29 23:42:57 -0400

    song list stateless function component

---
+++ react-context-hooks/hooksapp/src/components/SongList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>ventia</li>
+                <li>secure the bag</li>
+            </ul>
+        </div>
+    )
+}
