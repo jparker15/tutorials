8ff1715c7747475dbab7745302e7c793d3308adc
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-30 00:08:28 -0400

    import uuid, useState to set default songs as current state, setSongs adds a new obj with a static title and unique id

--- react-context-hooks/hooksapp/src/components/SongList.js
+++ react-context-hooks/hooksapp/src/components/SongList.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {v1} from 'uuid';
 
 export const SongList = () => {
+    const [songs, setSongs] =  useState([
+        {title:"ventia",id:1},
+        {title:"secure the bag",id:2},
+        {title:"hatchback",id:3}
+    ]);
+    const addSong = () =>{
+        setSongs([...songs, {title: "new song", id:v1()}]);
+    }
     return (
         <div className="song-list">
             <ul>
-                <li>ventia</li>
-                <li>secure the bag</li>
+                {songs.map(song=>{
+                    return(<li key={song.id}>{song.title}</li>)
+                })}
             </ul>
+            <button onClick={addSong}>Add + </button>
         </div>
     )
 }
