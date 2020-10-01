02d7650329afd8388caf23f5ee74446f4af29221
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-01 11:15:03 -0400

    use BookContext, map through books return list items

--- react-context-hooks/contextapp/src/Components/Booklist.js
+++ react-context-hooks/contextapp/src/Components/Booklist.js
@@ -1,4 +1,5 @@
 import React, {/* Component,*/ useContext } from 'react'
+import { BookContext } from '../contexts/BookContext'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 // Class Component
@@ -26,13 +27,20 @@ import { ThemeContext } from '../contexts/ThemeContext'
 // Functional Component
 const Booklist = () => {
     const {isLightTheme,light, dark} = useContext(ThemeContext)
+    const {books} = useContext(BookContext)
     const theme = isLightTheme? light : dark;
      return (
          <div className="book-list" style={{color:theme.syntax,background:theme.bg}}>
                  <ul>
-                     <li style={{background:theme.bg}}>the name of the wind</li>
-                     <li style={{background:theme.bg}}>ender's game</li>
-                     <li style={{background:theme.bg}}> absolute sandman</li>
+                     {books.map(book =>{
+                         return(
+                         <li key={book.id}style={{background:theme.bg}} >{book.title}</li>
+                         )
+                     })}
+
+                     <li style={{background:theme.bg}}>The Name of The Wind</li>
+                     <li style={{background:theme.bg}}>Ender's Game</li>
+                     <li style={{background:theme.bg}}> Absolute Sandman Vol. 1</li>
                  </ul>
              </div>
      )
