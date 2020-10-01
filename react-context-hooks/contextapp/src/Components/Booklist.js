d5f073c13752358928806516c6df8c23d79221d9
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-01 10:32:01 -0400

    use functional comp instead of class

--- react-context-hooks/contextapp/src/Components/Booklist.js
+++ react-context-hooks/contextapp/src/Components/Booklist.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react'
+import React, {/* Component,*/ useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-export default class Booklist extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const {isLightTheme,light,dark} = this.context;
-        const theme = isLightTheme ? light: dark;
-        return (
-            <div className="book-list"
-                style={{color:theme.syntax,background:theme.bg}}
-            >
-                <ul>
-                    <li style={{background:theme.bg}}>the name of the wind</li>
-                    <li style={{background:theme.bg}}>ender's game</li>
-                    <li style={{background:theme.bg}}> absolute sandman</li>
-                </ul>
-            </div>
-        )
-    }
-}
+// Class Component
+// export default class Booklist extends Component {
+
+//     static contextType = ThemeContext;
+//     render() {
+//         const {isLightTheme,light,dark} = this.context;
+//         const theme = isLightTheme ? light: dark;
+//         return (
+//             <div className="book-list"
+//                 style={{color:theme.syntax,background:theme.bg}}
+//             >
+//                 <ul>
+//                     <li style={{background:theme.bg}}>the name of the wind</li>
+//                     <li style={{background:theme.bg}}>ender's game</li>
+//                     <li style={{background:theme.bg}}> absolute sandman</li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+
+// Functional Component
+export const Booklist = () => {
+    const {isLightTheme,light, dark} = useContext(ThemeContext)
+    const theme = isLightTheme? light : dark;
+     return (
+         <div className="book-list" style={{color:theme.syntax,background:theme.bg}}>
+                 <ul>
+                     <li style={{background:theme.bg}}>the name of the wind</li>
+                     <li style={{background:theme.bg}}>ender's game</li>
+                     <li style={{background:theme.bg}}> absolute sandman</li>
+                 </ul>
+             </div>
+     )
+ }
