2784ae22c4cf4ed6f15da5b9d998dea45cf12d44
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-05 13:38:20 -0400

    remove useState -> useReducer, change state to empty array, reduced add/removeBook functions

--- react-context-hooks/booklist/src/contexts/BookContext.js
+++ react-context-hooks/booklist/src/contexts/BookContext.js
@@ -1,23 +1,13 @@
-import React,{createContext, useState} from 'react'
-import {v1} from "uuid";
+import React,{createContext, useReducer} from 'react'
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) =>{
-    const [books, setBooks] = useState([
-        {title:"Name of the Wind",author:"Patrick Rothfuss", id: 1},
-        {title:"The Wise Man's Fear",author:"Patrick Rothfuss", id: 2}
-    ]);
-
-    const addBook = (title,author) =>{
-        setBooks([...books,{title,author, id:v1()}]);
-    };
-    const removeBook = (id) =>{
-        setBooks(books.filter((book => book.id !== id)))
-    };
+    const [books, dispatch] = useReducer(bookReducer, []);
 
     return (
-        <BookContext.Provider value={{books,addBook,removeBook}}>
+        <BookContext.Provider value={{books,dispatch}}>
             {props.children}
         </BookContext.Provider>
     )
